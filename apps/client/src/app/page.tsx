b95ceef2d5832c0e1e004d1833f356b78aa6767e
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2025-06-02 14:45:49 +0300

    m

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,15 +1,6 @@
 import Landing from '@/pages/Landing';
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [data, setData] = useState<any>(null);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/data')
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error);
-  }, []);
 
   return (
     <div>
